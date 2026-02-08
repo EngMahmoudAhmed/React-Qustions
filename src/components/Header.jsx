416ad748c97e310b63895138f5f88d74cdd0ed12
Mahmoud Ahmed <lya440676@gmail.com>
2026-02-08 10:39:28 +0200

    feat: Update Header component to display question status and improve layout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,22 @@
+import useQuiz from "../context/useQuiz";
 import ReactLogo from "/react.png";
 
 const style = {
-  marginTop: "10px",
   display: "flex",
   alignItems: "center",
   justifyContent: "space-evenly",
+  flexDirection:"column",
 };
 
 function Header() {
+  const {status, index, questions } = useQuiz()
   return (
     <div style={style}>
       <img src={ReactLogo} alt="React logo" width={100} />
       <h1>The React Quiz</h1>
+      {status === "active" && (
+        <p>Question {index + 1} of {questions.length}</p>
+      )}
     </div>
   );
 }
